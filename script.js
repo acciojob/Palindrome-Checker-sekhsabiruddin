516ebf4b6887740a73769b7031017422cf112d3b
sk sabiruddin <120248500+sekhsabiruddin@users.noreply.github.com>
2023-01-12 12:55:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+    let start =0;
+	let end=str.length-1;
+	while(start]<=str[end]){
+		if(str[start]!=str[end]) return false;
+				start++;
+		end--;
+	}
+	return ture;
 }
 module.exports = palindrome
